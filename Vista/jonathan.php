ae2700fd3dda44c8a1eca4fcd45560ac3a393b61
Jonathan goiz <jonathanga@gmail.com>
2026-09-25 09:43:25 -0600

    agregado de mi pagina

---
+++ Vista/jonathan.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="js/pie.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <!--encabezado-->
+        <?php include_once("../public/encabezado.php") ?>
+        <!--fin encabezado-->
+    </header>
+
+    <!--inicia el cuerpo de  la  página-->
+    <div class="container" style="text-align: center;" >
+        <h2> jonathan Goiz Arana</h2>
+    </div>
+    <!--termina  cuerpo de  la  pagina-->
+
+
+    <!-- INICIA  CAROUSEL-->
+    <div class="card mx-auto text-center" style="width: 18rem;">
+  <img src="../img/im.jpeg" class="card-img-top" alt="...">
+  <div class="card-body" style="text-align: center;">
+    <h5 class="card-title">Bienvenidos a mi pagina Jonathan</h5>
+    <p class="card-text">mi nombre es jonathan y yo desarrollo paginas web para la clinica veterinaria</p>
+  </div>
+</div>
+
+    <!-- FIN  CAROUSEL-->
+
+
+
+    <!-- <footer>
+        <!-- inicia pie-->
+    <?php include_once("../public/pie.php") ?>
+    <!--fin pie-->
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
