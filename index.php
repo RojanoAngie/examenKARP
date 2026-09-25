0b2175e392bbf7069a4ae56dc566c88c4835f088
Karla angelica Rojano <l23240010@smartin.tecnm.mx>
2026-09-25 09:12:50 -0600

    examen_karp

---
+++ index.php
@@ -0,0 +1,50 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="js/pie.css" rel="stylesheet">
+</head>
+
+<body>
+
+    <header>
+        <!--encabezado-->
+        <?php include_once("public/encabezado.php") ?>
+        <!--fin encabezado-->
+    </header>
+
+    <!--inicia el cuerpo de  la  página-->
+    <div class="container" style="text-align: center;">
+        <h2> !!! BIENVENIDOS !!</h2>
+    </div>
+    <!--termina  cuerpo de  la  pagina-->
+
+
+    <!-- INICIA  CAROUSEL-->
+    <div class="card" style="width: 18rem;">
+  <img src="img/coati.jpg" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">Bienvenidos a la pagina de Karla</h5>
+    <p class="card-text">este es un coatí</p>
+  </div>
+</div>
+
+    <!-- FIN  CAROUSEL-->
+
+
+
+    <!-- <footer>
+        <!-- inicia pie-->
+    <?php include_once("public/pie.php") ?>
+    <!--fin pie-->
+    </footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
---
+++ public/pie.php
@@ -0,0 +1,27 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container-fluid" style="text-align: center; padding-top:10px; background-color:#e5e5e5">
+        <div class="row">
+            <div class="col">
+                <pre style="padding-top: 70px;">
+              @elaborado por KARP
+                </pre>
+            </div>
+
+        </div>
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
